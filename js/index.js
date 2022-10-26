674516cd58c1b6572e37687eec231d5fbb9bd807
Marga <margalppalacios@gmail.com>
2022-10-26 16:38:14 +0200

    añadir productos al carrito se muestra por consola

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const clickButton = document.querySelectorAll('.button-compra')
-
+let carrito = []
 
 clickButton.forEach(btn => {
     btn.addEventListener('click', addCarritoItem)
@@ -8,4 +8,9 @@ clickButton.forEach(btn => {
 function addCarritoItem(e){
     const button = e.target
     const item = button.closest(".card")
+    const itemTittle = item.querySelector('.card-tittle').textContent;
+    const itemPrice = item.querySelector('.precio').textContent;
+    const itemImg = item.querySelector('.top-bestseller').src;
+    console.log(itemImg)
+    
 }
