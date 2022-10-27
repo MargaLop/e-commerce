d2c0626e29df9fc0b5763cbbe781075199e7434a
Marga <margalppalacios@gmail.com>
2022-10-27 15:43:07 +0200

    añadir items carrito

--- js/index.js
+++ js/index.js
@@ -29,8 +29,11 @@ function addItemCarrito(newItem){
     for(let i =0; i < carrito.length ; i++){
         if(carrito[i].title.trim() === newItem.title.trim()){
           carrito[i].cantidad ++;
-            console.log(carrito)
+          const inputValue = inputElemnto[i]
+          inputValue.value ++;
+          carritoTotal()
           return null;
+
         }
     }
     carrito.push(newItem)
@@ -46,16 +49,30 @@ function renderCarrito(){
         const content = `
             <th scope="row">1</th>
                 <td class="table__productos">
+                <img src=${item.img} alt="" class="item-imagen__card">
                     <h6 class="title">${item.title}</h6>
-                    <img src=${item.img} alt="" class="item-imagen__card">
                 </td>
                 <td class="table__price"><p>${item.precio}</p></td>
-                <td class="table__cantidad>
-                <input type="number" min="1" value=${item.cantidad} class="input__elemento">
-                    <buttom class="delete btn btn-danger">x</buttom>
+                <td class="table__cantidad">
+                    <input type="number" min="1" value=${item.cantidad} class="input__elemento">
+                    <button class="delete btn btn-danger">x</button>
                 </td>
         `
         tr.innerHTML = content
         tbody.append(tr)
+        
+        tr.querySelector(".delete").addEventListener('click', removeItemCarrito)
+
     })
+    carritoTotal()
+}
+
+function carritoTotal(){
+    let total = 0
+    const itemCartTotal = document.querySelector('.itemCartTotal')
+    carrito.forEach((item)=>{
+        const precio = Number(item.precio.replace("$",''))
+        total = total + + precio*item.cantidad
+})
+    itemCartTotal.innerHTML = `Total ${total}$ `
 }
